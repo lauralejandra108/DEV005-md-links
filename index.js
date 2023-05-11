1cef88d98808d542c01b99c552128a9e853e07f3
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-05-11 18:28:00 -0500

    validar ruta, convertir ab, mostrar archivo .md

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = () => {
-  // ...
-};
+
+const colors = require('colors');
+const fs = require('fs');
+const path = require('path');
+/*fs.readFile('README.md', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});*/
+
+// Validar si existe una ruta
+const route = '/Users/LauraAlejandra/Documents/YOGA BOOK';
+//const route = '/Users/LauraAlejandra/Documents/DesarrolloW/LABORATORIA/data-lovers-pokemon';
+let fileExists = fs.existsSync(route);
+console.log('exists:', fileExists);
+
+//Convertir a ruta absoluta
+console.log(path.join(route));
+
+//
+
+//Mostrar los arhivos con extencion .md de un directorio 
+files = fs.readdirSync(route);
+console.log('/Filenames with the .md extension:');
+files.forEach(file => {
+  if (path.extname(file) === '.md') {
+    console.log(file);
+  }
+   else {console.log('Esta carpeta no contiene archivos.md'.red);}
+});
+
+// Extraer links de archivos
