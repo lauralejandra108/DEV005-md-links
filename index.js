45b9bee3f1b655871b301147e5e1848877f07119
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-05-18 19:28:18 -0500

    Se creo funcion que lee los archivos y trae los links de las carpetas y los guarda en un arry

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const colors = require('colors');
 const fs = require('fs');
 const path = require('path');
+const markdownIt = require('markdown-it');
+const { JSDOM } = require('jsdom');
+const route = process.argv[2];
 // Validar si existe una ruta
 //const route = '/Users/LauraAlejandra/Documents/YOGA BOOK';
-const route = '/Users/LauraAlejandra/Documents/pruebaMdL';
+/* const route = '/Users/LauraAlejandra/Documents/pruebaMdL'; */
 let fileExists = fs.existsSync(route);
 console.log('exists:', fileExists);
 
@@ -26,11 +29,62 @@ const recursive = (route) => {
 }
 return  arryMd.filter(file => path.extname(file) === '.md');
  }
-console.log('funciana', recursive(route));
+ const arryMd = recursive(route);
+console.log('funciona', recursive(route));
 
 
 //Convertir a ruta absoluta
 const absolutePath = path.resolve(route);
 console.log( 'Ruta Absoluta'.bgMagenta, absolutePath);
 
+// Leer los arhivos 
+ const readMd = (arryMd) => 
+ new Promise((resolve, reject) => {
+  fs.readFile( arryMd, 'utf8', (err, data) => {
+    if (err) reject(new Error(err));
+      resolve(data);
+  });
+ });
+  const readMds = (arryMd) =>{
+    return Promise.all(arryMd.map((element) => readMd(element)))
+    .then((results) => {
+      
+      console.log(getLinks(results) ); 
+    })
+    .catch((error) => {
+  console.error(error);
+  });
+   };
+     readMds(arryMd);
+   
 // Extraer links de archivos
+const getLinks = (file) => {
+  let allLinks = [];
+  file.forEach((e) => {
+  const md = new markdownIt();
+  const content = md.render(e);
+  const dom = new JSDOM(content);
+  const { document } = dom.window;
+  const links = document.querySelectorAll('a');
+
+    links.forEach((link) => {
+      const href = link.getAttribute('href');
+      const text = link.textContent;
+      const file = route;
+      if (href.startsWith('https')){
+      allLinks.push({ href, text, file });
+      }
+     });
+    });
+ return allLinks;
+  };
+  
+
+/* const getLinks = (data) => {
+  let url =  /\[([^\[\]]*?)\]\((https?:\/\/[^\s$.?#].[^\s]*)\)/g ;
+  let arrayL = data.toString().match(url)
+if(arrayL && arrayL!== undefined){
+    return  arrayL  
+}
+};
+console.log('Esto me trae links', getLinks(arryMd)); */
